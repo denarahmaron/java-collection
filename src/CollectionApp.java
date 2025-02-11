cfe21d244e6feaa427a7ae103477ccf2474742e2
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-02-11 23:59:05 +0700

    add collection

---
+++ src/CollectionApp.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class CollectionApp {
+    public static void main(String[] args) {
+        Collection<String> names = new ArrayList<>();
+        names.add("Denar");
+        names.add("Ahmaron");
+        names.addAll(Arrays.asList("Surya", "Gemilang"));
+
+        names.remove("Denar");
+        names.removeAll(Arrays.asList("Surya", "Gemilang"));
+
+        for(var name : names){
+            System.out.println(name);
+        }
+
+        System.out.println(names.contains("Ahmaron"));
+    }
+}
