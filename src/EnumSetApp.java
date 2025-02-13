ffd5a502071ea9f85e7f1afb22e3cdff872c801b
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-02-13 23:37:54 +0700

    add set

---
+++ src/EnumSetApp.java
@@ -0,0 +1,14 @@
+import java.util.EnumSet;
+
+public class EnumSetApp {
+    public static enum Gender{
+        MALE, FEMALE, NOT_MENTION
+    }
+
+    public static void main(String[] args) {
+        EnumSet<Gender> genders = EnumSet.allOf(Gender.class);
+        for(var gender : genders){
+            System.out.println(gender);
+        }
+    }
+}
