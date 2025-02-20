c3eaacb5d4382d991d51b7ba97ad221ab4976d8e
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-02-21 00:03:11 +0700

    add array convertion

---
+++ src/ArrayApp.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayApp {
+    public static void main(String[] args) {
+        List<String> names = List.of("Denar", "Ahmaron", "Surya");
+
+        Object[] objects = names.toArray();
+        String[] strings = names.toArray(new String[]{});
+
+        System.out.println(Arrays.toString(objects));
+        System.out.println(Arrays.toString(strings));
+    }
+}
